1254bc81f05b45fb4ef99884a5bc52988617514d
Kunal <kunal@edulab.in>
2022-09-27 15:29:11 +0530

    direct commit

--- ssss.js
+++ ssss.js
@@ -5,5 +5,8 @@ const app = express()
 app.get('/', function (req, res) {
   res.send('Hello Worlsd122d fghh')
 })
+app.post('/', function (req, res){
+    res.send('hello world')
+})
 
-app.listen(3000)
+app.listen(5000)
